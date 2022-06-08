68bf6f74681a12f7cfa5baa83b629b91575dff7c
GabrielMSosa <86896491+GabrielMSosa@users.noreply.github.com>
2022-06-08 20:28:48 -0300

    first commit

---
+++ src/main/java/com/QRSearch/QRSearch/QrSearchApplication.java
@@ -0,0 +1,13 @@
+package com.QRSearch.QRSearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/QRSearch/QRSearch/QrSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.QRSearch.QRSearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
